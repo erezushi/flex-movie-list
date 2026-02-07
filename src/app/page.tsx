4a608f52747e635ee167d0c8f4be5e1de9be7b16
PokéErez <erezushi900@gmail.com>
2026-02-07 18:41:32 +0200

    Added AppSelector hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 'use client';
 
-import { useAppDispatch } from '@/store/hooks';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
 
 import styles from './page.module.css';
 
 export default function Home() {
   const dispatch = useAppDispatch();
+  const { displayMode, currentPage } = useAppSelector((state) => {
+    const { displayMode, currentPage } = state.movies;
+
+    return { displayMode, currentPage };
+  });
 
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        
+      </main>
     </div>
   );
 }
-
--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,4 +1,6 @@
-import { useDispatch } from "react-redux";
-import { AppDispatch } from ".";
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '.';
+import { RootState } from './rootReducer';
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
